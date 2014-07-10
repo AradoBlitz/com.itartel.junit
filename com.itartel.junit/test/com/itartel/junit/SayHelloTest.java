832d4e3d95b597a6a8bda55186a3a244ff8bd00e
AradoBlitz <schlachtnavaho@gmail.com>
2014-07-10 13:24:53 +0300

    Before/After annotations example.

--- com.itartel.junit/test/com/itartel/junit/SayHelloTest.java
+++ com.itartel.junit/test/com/itartel/junit/SayHelloTest.java
@@ -2,13 +2,45 @@ package com.itartel.junit;
 
 import static org.junit.Assert.*;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.core.IsEqual;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class SayHelloTest {
 
+	@BeforeClass
+	public static void setUpClass(){
+		System.out.println("Before Class");
+	}
+	
+	@AfterClass
+	public static void tearDownClass(){
+		System.out.println("After Class");
+	}
+	
+	@Before
+	public void setUp(){
+		System.out.println("Before");
+	}
+	
+	@After
+	public void tearDown(){
+		System.out.println("After");
+	}
+	
 	@Test
-	public void sayHello() throws Exception {
+	public void sayHelloAssert() throws Exception {
 		Greeting greeting = new Greeting("World"); 
 		assertEquals("Hello World!",greeting.sayHello());
+	}
+	
+	@Test
+	public void sayHelloAssertThat() throws Exception {
+		Greeting greeting = new Greeting("World");
+		assertThat(greeting .sayHello(), new IsEqual<String>("Hello World!") );
 	}
 }
