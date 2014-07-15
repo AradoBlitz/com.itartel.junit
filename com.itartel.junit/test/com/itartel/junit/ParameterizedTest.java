0b40f005f06aefdc06d5001dd11a2707820a44a3
AradoBlitz <schlachtnavaho@gmail.com>
2014-07-15 12:51:14 +0300

    Constructor parameterized test.

--- com.itartel.junit/test/com/itartel/junit/ParameterizedTest.java
+++ com.itartel.junit/test/com/itartel/junit/ParameterizedTest.java
@@ -2,14 +2,32 @@ package com.itartel.junit;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class ParameterizedTest {
 
+	@Parameters
+	public static Collection<Object[]> data(){
+		return Arrays.asList(new Object[][]{{1,0},{2,1},{3,2}});
+	}
+	
+	private final int input;
+	private final int expected;
+	
+	public ParameterizedTest(int expected, int input){
+		this.input = input;
+		this.expected = expected;
+	}
+	
 	@Test
 	public void addOneToNumber() throws Exception {
-		assertEquals(1,1+0);
-		assertEquals(2,1+1);
-		assertEquals(3,1+2);
+		assertEquals(expected,1+input);
 	}
 }
